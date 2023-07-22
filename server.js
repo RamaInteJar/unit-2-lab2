3d82393d8abeb1101b8861934a45e2882a205482
pentiumKing <ramahawai87@gmail.com>
2023-07-21 21:52:44 -0400

    added comments on calculator route

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ app.get("/greeting/:name", (req, res)=>{
 })
 
 app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    // evoking the total  from the request parameters
     const total = parseInt(req.params.total);
+     // evoking the tipPercentage from the request parameters
     const tipPercentage = parseInt(req.params.tipPercentage)
+    //Calculate the total tip amount
     const tipAmount = ((total*tipPercentage)/100).toFixed(2)
-
+    //Send the tip amount as response
     res.send(`Your tip amount is: ${tipAmount}`)
 })
 
