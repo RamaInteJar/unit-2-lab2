66b73d58727850d13e8c3900d9f16a3bff7bbe29
pentiumKing <ramahawai87@gmail.com>
2023-07-21 21:41:04 -0400

    Added a calculator route

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get("/greeting/:name", (req, res)=>{
     res.send(`Hello, ${name} ! it is so great to see you`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    const total = parseInt(req.params.total);
+    const tipPercentage = parseInt(req.params.tipPercentage)
+    const tipAmount = ((total*tipPercentage)/100).toFixed(2)
+
+    res.send(`Your tip amount is: ${tipAmount}`)
+})
+
 app.listen(3000, ()=>{
     console.log("3000 are you listening?");
 })
