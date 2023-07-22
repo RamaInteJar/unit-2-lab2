2c4c34b2b48d0cb730abf015062df8a65f9bf400
pentiumKing <ramahawai87@gmail.com>
2023-07-21 23:05:55 -0400

    added a new magic route

--- server.js
+++ server.js
@@ -18,6 +18,28 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
     res.send(`Your tip amount is: ${tipAmount}`)
 })
 
+
+const magic8Responses = [
+  "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+  "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+  "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+  "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+  "Don't count on it", "My reply is no", "My sources say no",
+  "Outlook not so good", "Very doubtful"
+];
+
+// Define the route '/magic/:question'
+app.get('/magic/:question', (req, res) => {
+  // Extract the question from the URL 
+  const question = req.params.question
+
+  // Get a random Magic 8 ball response from the array
+  const randomMagic8Response = magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
+
+  // Send the question and the Magic 8 ball response as a response
+  res.send(`<h1>Your Question: ${question}</h1><h1>Magic 8 Ball Response: ${randomMagic8Response}</h1>`);
+});
+
 app.listen(3000, ()=>{
     console.log("3000 are you listening?");
 })
